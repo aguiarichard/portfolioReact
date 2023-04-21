8de8994a8cc57bb33f104f02ff455d750088590d
Richard de aguiar <richard_aguiar.19@hotmail.com>
2023-04-21 03:05:40 -0300

    implementando NavItem

--- src/pages/Components/Nav.jsx
+++ src/pages/Components/Nav.jsx
@@ -1,17 +1,15 @@
 import React from "react"
 
+import NavItem from "./Nav-item"
+
 import '../../styles/nav.css'
 
 export default function Nav() {
   return (
     <nav id="nav" className="nav">
-      <a href="/projetos">Projetos</a>
-      <a href="/sobre">Sobre</a>
-      <a href="/contatos">Contato</a>
+      <NavItem href="projetos" name="Projetos"/>
+      <NavItem href="sobre" name="Sobre"/>
+      <NavItem href="contatos" name="Contatos"/>
     </nav>
   )
 }
-
-{/* <NavItem href="projetos" name="Projetos"/>
-<NavItem href="sobre" name="Sobre"/>
-<NavItem href="contatos" name="Contatos"/> */}
