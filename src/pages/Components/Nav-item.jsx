0a7f15ec4877f3e3c53e692617c113af54514b64
Richard de aguiar <richard_aguiar.19@hotmail.com>
2023-04-21 03:26:09 -0300

    criando função para fechar navegação

--- src/pages/Components/Nav-item.jsx
+++ src/pages/Components/Nav-item.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function NavItem(props) {
+    function fecharNav() {
+        const nav = document.querySelector('#nav')
+    
+        nav.style.transform = 'translateY(-900px)'
+      }
+
     return (        
-        <Link to={`/${props.href ? props.href : ''}`}>
+        <Link to={`/${props.href ? props.href : ''}`} onClick={fecharNav}>
             {props.name}
         </Link>
     )
