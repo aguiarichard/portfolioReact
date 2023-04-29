45e99628bc4137e9c6f251ef2792763e3fcf01ef
Richard de aguiar <richard_aguiar.19@hotmail.com>
2023-04-28 23:34:22 -0300

    alterações no botão burger

--- src/pages/Components/Nav.jsx
+++ src/pages/Components/Nav.jsx
@@ -7,8 +7,8 @@ import '../../styles/nav.css'
 export default function Nav() {
   return (
     <nav id="nav" className="nav">
-      <NavItem href="projetos" name="Projetos"/>
       <NavItem href="sobre" name="Sobre"/>
+      <NavItem href="projetos" name="Projetos"/>
       <NavItem href="contatos" name="Contatos"/>
     </nav>
   )
