14c19ff00b639979de9b85867758a9d1bdd5fb2b
Richard de Aguiar da Silva <01394962037@canoas.unilasalle.edu.br>
2024-01-05 17:37:58 -0300

    adicionando tela de carregamento logo principal

--- src/pages/Components/Header.jsx
+++ src/pages/Components/Header.jsx
@@ -2,26 +2,46 @@ import React from "react";
 import Nav from "./Nav";
 
 import '../../styles/header.css'
+import { Link } from "react-router-dom";
 
 export default function Header() {
   function burgerView() {
     const burger = document.querySelector('#burger')
     burger.classList.toggle('burger-open')
-    
+
     const nav = document.querySelector('#nav')
 
     if (nav.style.transform == 'translateY(0px)') {
       nav.style.transform = 'translateY(-1000px)'
       return
     }
-    
+
     nav.style.transform = 'translateY(0)'
+  }
+
+  function carregamento(e) {
+    const body = document.querySelector('body')
+    const div = document.querySelector('.carregamento')
+
+    body.appendChild(div)
+
+    div.style.transform = `translateX(${div.offsetWidth - 200}px)`
+
+    setTimeout(() => {
+      div.style.transform = `translateX(0)`
+
+    }, 800)
+
+    console.log(body, div.offsetWidth, e);
   }
 
   return (
     <header className="header">
       <div>
-        <h1>Richard de Aguiar</h1>
+        <Link to={`/`} onClick={carregamento}>
+          <h1>Richard de Aguiar</h1>
+        </Link>
+        <div className="carregamento"></div>
         <div id="burger" className="burger" onClick={burgerView}>
           <i></i>
           <i></i>
